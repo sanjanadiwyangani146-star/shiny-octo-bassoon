e7dac6b57aaf5382e20e85931e4b8abf5e1354ea
sanjanadiwyangani146-star <sanjanadiwyangani146@gmail.com>
2026-04-06 16:01:47 +0530

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,12 @@
-import { Navbar } from "../components/Navbar";
-import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "@/components/StarBackground";
-import { HeroSection } from "../components/HeroSection";
-import { AboutSection } from "../components/AboutSection";
-import { SkillsSection } from "../components/SkillsSection";
-import { ProjectsSection } from "../components/ProjectsSection";
-import { ContactSection } from "../components/ContactSection";
-import { Footer } from "../components/Footer";
+import Navbar from "../components/Navbar";
+import ThemeToggle from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import SkillsSection from "../components/SkillsSection";
+import ProjectsSection from "../components/ProjectsSection";
+import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 
 export const Home = () => {
   return (
